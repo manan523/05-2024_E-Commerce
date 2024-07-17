e05f855b1cc061c10f4d5b7ca99fb845fbd8f19f
manan523 <129627596+manan523@users.noreply.github.com>
2024-07-18 01:52:49 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -23,7 +23,7 @@ export const fetchAllOrders = async (req, res) => {
   }
   try {
     const result = await query.exec();
-    const totalCount = await query2.count().exec();
+    const totalCount = await query2.countDocuments();
     res.set("X-Total-Count", totalCount);
     res.status(200).json(result);
   } catch (err) {
