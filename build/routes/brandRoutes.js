db045299a1874a8f3b2c3ba8e490c412c9b976ea
manan523 <129627596+manan523@users.noreply.github.com>
2024-07-29 13:30:51 +0530

    Delete build/routes directory

--- build/routes/brandRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import {fetchBrands } from "../controllers/brandController.js";
-const router = express.Router();
-
-router.get("/", fetchBrands);
-
-export default router;
--- build/routes/categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import {fetchCategories } from "../controllers/categoryController.js";
-
-const router = express.Router();
-
-router.get("/", fetchCategories);
-
-export default router;
